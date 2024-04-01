14153f30a23101997b1dd41617647c79d38fd89d
nalisherovvna <mushknasiba@gmail.com>
2024-04-01 16:54:33 +0500

    it was my homeowrk

---
+++ main.js
@@ -0,0 +1,59 @@
+// UY ISHI
+// 1) Ikkita o'zgaruvchi yarating va ularning ichiga son saqlang va bir birini cosole.logda qo'shib chiqaring chiqaring
+
+// let sport = 5;
+// let sport2 = 4;
+// console.log(sport+sport2);
+
+
+
+// 2) let a  = '12' shu 12 sonini data typeni o'zgartirib number qilib chiqaring 
+// bering. Yani bu hozir string typeda siz number tyga o'zgartiring
+
+// let a = +'12';
+// console.log(a);
+
+
+
+// 3) randomli son yasang va shu randomli sonning juft yoki toq ekanligini aniqlab bering
+
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// if (randomNumber % 2 === 0) {
+//     alert(randomNumber + " soni juft");
+// } else {
+//     alert(randomNumber + " soni toq");
+// }
+
+
+// 4) promptdan bitta so'z kiriting agar o'sha so'z Mars so'ziga teng bolsa o'sha so'zning
+//  o'zi chiqsin agar boshqa so'z bo'lsa Mars It school deb chiqsin
+
+// let word = prompt("");
+
+// if (word === "Mars") {
+//     alert(word)
+// } else {
+//     alert("Mars It school")
+// }
+
+
+// 5) bitta array yarating va uning ichiga 5 ta so'z kiriting. Keyin For yordamida o'sha arraydagi 
+// so'zlar ichidan uzunligi 5 va undan kattalarini bohsqa bir arrayga qo'shib chiqaring
+
+
+
+
+// 6) for orqali birdan 100gacha bolgan sonlarni juft yoki toqqa ajratib chiqaring
+
+// function checkEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "juft";
+//     } else {
+//         return "toq";
+//     }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i + " " + checkEvenOrOdd(i));
+// }
